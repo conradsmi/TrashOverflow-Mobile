7e15b9479830a80461179d0589451a3ca594eb15
conradsmi <conradsmith22@protonmail.com>
2021-04-21 18:30:47 -0400

    removed extraneous expo init code

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
@@ -14,12 +12,3 @@ export default function App() {
     />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
