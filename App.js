8951773d1ec9ff23d13383f40b6ade9fe1b9d773
conradsmi <conradsmith22@protonmail.com>
2021-04-21 18:28:03 -0400

    automatically forward user to signin page on startup

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-// test2
 export default function App() {
   return (
+    // Forward you to sign in page
     <WebView
-      source={{ uri: 'https://trash-overflow-g8.herokuapp.com' }}
+      source={{ uri: 'https://trash-overflow-g8.herokuapp.com/signin' }}
       style={{ marginTop: '10%' }}
       sharedCookiesEnabled={true}
       thirdPartyCookiesEnabled={true}
